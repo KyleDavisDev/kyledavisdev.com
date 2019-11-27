9cea35eddf0f12fecca9519b355e0553842cf247
Kyle Bonar <kylewb2011@gmail.com>
2019-11-27 11:35:43 -0600

    Init commit

---
+++ src/Screens/Home/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
+import { StyledContainer, StyledTextContainer } from "./AboutMeStyles";
+
+interface IAboutMeProps {}
+
+const AboutMe: React.FunctionComponent<IAboutMeProps> = props => {
+  return (
+    <StyledContainer>
+      <StyledTextContainer>
+        <SectionTitle>About me</SectionTitle>
+        <p>Hello world! Welcome to my portfolio website.</p>
+
+        <p>
+          I am a <span className="bold">Full Stack Developer</span> that enjoys
+          working with JavaScript as much as possible. You will, however, often
+          find me learning new languages as well!
+        </p>
+
+        <p>
+          I primarily focus on the front-end, where I make pixel-perfect,
+          minimalistic designs, but I also enjoy working with databases and
+          server logic. In my projects page, for example, you can find several
+          applications where I wrote the entire stack!
+        </p>
+
+        <p>
+          The majority of the updates to this site will be focused on the
+          projects page. Stay tuned!
+        </p>
+
+        <p>-Kyle Davis</p>
+      </StyledTextContainer>
+    </StyledContainer>
+  );
+};
+
+export default AboutMe;
---
+++ src/Screens/Home/components/AboutMe/AboutMeStyles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  margin: 0 auto;
+  max-width: 900px;
+  padding: 2em;
+`;
+
+export const StyledTextContainer = styled.div`
+  display: block;
+  max-width: 450px;
+
+  p {
+    font-family: DroidSans, Helvetica, Arial, sans-serif;
+    font-size: 1rem;
+    margin: 0 0 0.5em;
+  }
+`;
