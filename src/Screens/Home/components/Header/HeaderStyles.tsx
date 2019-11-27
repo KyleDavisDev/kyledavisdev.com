f646196ec942f87626dc291a71c88d65fc10f148
Kyle Bonar <kylewb2011@gmail.com>
2019-11-27 14:25:03 -0600

    Remove unused styling

--- src/Screens/Home/components/Header/HeaderStyles.tsx
+++ src/Screens/Home/components/Header/HeaderStyles.tsx
@@ -1,19 +1,5 @@
 import styled from "../../../../theme/styled-components";
 
-export const StyledContainer = styled.div`
-  padding: 2rem;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
-  background-color: rgba(0, 160, 227, 0.05);
-`;
-
-export const StyledHeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-top;
-  max-width: 900px;
-  margin: 0 auto;
-`;
-
 export const StyledImageContainer = styled.a`
   text-decoration: none;
   display: inline-block;
