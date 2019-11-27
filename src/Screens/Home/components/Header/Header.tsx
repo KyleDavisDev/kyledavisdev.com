c10bc2818dd99af24cbc162885fb09e2ce55493e
Kyle Bonar <kylewb2011@gmail.com>
2019-11-27 14:24:52 -0600

    Utilize Bar component

--- src/Screens/Home/components/Header/Header.tsx
+++ src/Screens/Home/components/Header/Header.tsx
@@ -2,36 +2,33 @@ import * as React from "react";
 
 import profilePic from "../../../../images/profile/profile-pic.jpg";
 import {
-  StyledContainer,
   StyledImageContainer,
   StyledImage,
-  StyledHeaderContainer,
   StyledTextContainer,
   StyledName,
   StyledAttribute
 } from "./HeaderStyles";
+import Bar from "../../../../components/Bar/Bar";
 
 export interface IHeaderProps {}
 
 export default class Header extends React.PureComponent<IHeaderProps> {
   public render() {
     return (
-      <StyledContainer>
-        <StyledHeaderContainer>
-          <StyledImageContainer href="#">
-            <StyledImage src={profilePic} />
-          </StyledImageContainer>
-          <StyledTextContainer>
-            <StyledName>Kyle Davis</StyledName>
-            <StyledAttribute>
-              <span>Full Stack Developer</span>
-            </StyledAttribute>
-            <StyledAttribute>
-              Practicing <span>Coffee Drinker</span>
-            </StyledAttribute>
-          </StyledTextContainer>
-        </StyledHeaderContainer>
-      </StyledContainer>
+      <Bar>
+        <StyledImageContainer href="#">
+          <StyledImage src={profilePic} />
+        </StyledImageContainer>
+        <StyledTextContainer>
+          <StyledName>Kyle Davis</StyledName>
+          <StyledAttribute>
+            <span>Full Stack Developer</span>
+          </StyledAttribute>
+          <StyledAttribute>
+            Practicing <span>Coffee Drinker</span>
+          </StyledAttribute>
+        </StyledTextContainer>
+      </Bar>
     );
   }
 }
