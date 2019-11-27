c4fcf998a25fc42288c1d902b0a5ab02861714cb
Kyle Bonar <kylewb2011@gmail.com>
2019-11-27 14:14:29 -0600

    Bring in projects

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Nav } from "../../components/Nav/Nav";
 import Header from "./components/Header/Header";
 import AboutMe from "./components/AboutMe/AboutMe";
 import ContactMe from "./components/ContactMe/ContactMe";
+import Projects from "./components/Projects/Projects";
 
 export interface IHomeProps {}
 
@@ -14,6 +15,7 @@ export default class Home extends React.Component<IHomeProps> {
         <Header />
         <AboutMe />
         <ContactMe />
+        <Projects />
         <div>Hello world!!</div>
       </React.Fragment>
     );
