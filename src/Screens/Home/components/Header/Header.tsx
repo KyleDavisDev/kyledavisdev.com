1a918892cc73f838548a7a75400e303fa7f73276
Kyle Bonar <kylewb2011@gmail.com>
2019-11-28 00:02:40 -0600

    add contact content

--- src/Screens/Home/components/Header/Header.tsx
+++ src/Screens/Home/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
 import profilePic from "../../../../images/profile/profile-pic.jpg";
+import { FaEnvelope } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import {
   StyledImageContainer,
   StyledImage,
@@ -27,6 +30,26 @@ export default class Header extends React.PureComponent<IHeaderProps> {
           <StyledAttribute>
             Practicing <span>Coffee Drinker</span>
           </StyledAttribute>
+          <ul>
+            <li>
+              <a href="mailto:kyledavisdev@gmail.com">
+                <FaEnvelope /> Mail
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/kyledavisdev" target="_blank">
+                <FaGithub /> GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/kyle-davis-687276126/"
+                target="_blank"
+              >
+                <FaLinkedin /> LinkedIn
+              </a>
+            </li>
+          </ul>
         </StyledTextContainer>
       </Bar>
     );
