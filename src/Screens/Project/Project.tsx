4c8e63bc7c477467f08e269c6fbd8d29df671c2d
Kyle Bonar <kylewb2011@gmail.com>
2019-12-10 15:22:14 -0600

    Building out component

--- src/Screens/Project/Project.tsx
+++ src/Screens/Project/Project.tsx
@@ -1,16 +1,37 @@
 import * as React from "react";
 import { Nav } from "../../components/Nav/Nav";
 import Bar from "../../components/Bar/Bar";
+import { StyledProjectInfoContainer } from "./ProjectStyles";
 
 interface IProjectProps {}
 
-const Project: React.FunctionComponent<IProjectProps> = props => {
-  return (
-    <React.Fragment>
-      <Nav />
-      <Bar>Test contnet</Bar>
-    </React.Fragment>
-  );
-};
+interface IProjectState {
+  pageTitle: string;
+}
+
+class Project extends React.Component<IProjectProps, IProjectState> {
+  constructor(props: IProjectProps) {
+    super(props);
+
+    this.state = {
+      pageTitle: "Texas a&M"
+    };
+  }
+
+  public render() {
+    return (
+      <React.Fragment>
+        <Nav />
+        <Bar>
+          <h5>{this.state.pageTitle}</h5>
+          <a href="#" target="_blank" rel="nofollow">
+            View ->
+          </a>
+        </Bar>
+        <StyledProjectInfoContainer>content heer</StyledProjectInfoContainer>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Project;
