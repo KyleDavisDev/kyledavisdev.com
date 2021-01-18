7b93cd7b2d6a38b05fe98e7c1f219badefdef546
Kyle Bonar <kylewb2011@gmail.com>
2021-01-18 17:38:14 -0600

    Separated out tags to own styled component

--- src/components/Projects/ProjectsStyles.tsx
+++ src/components/Projects/ProjectsStyles.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/Link";
 import styled from "../../theme/styled-components";
 
 export const StyledContainer = styled.div`
@@ -9,47 +11,57 @@ export const StyledProjectsContainer = styled.div`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
+`;
 
-  > div {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    margin: 0 -15px;
+export const StyledGrid = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 0 -15px;
+`;
 
-    > div {
-      padding: 5px 10px 15px;
-      width: 100%;
-      max-width: 100%;
-      box-sizing: border-box;
+export const StyledProject = styled.div`
+  padding: 5px 10px 15px;
+  width: 100%;
+  max-width: 100%;
+  box-sizing: border-box;
+  position: relative;
 
-      // tablets
-      @media (min-width: ${props => props.theme.exToSm}) {
-        max-width: 50%;
-      }
+  // tablets
+  @media (min-width: ${props => props.theme.exToSm}) {
+    max-width: 50%;
+  }
 
-      // desktop
-      @media (min-width: ${props => props.theme.mdToLg}) {
-        max-width: 33%;
-      }
+  // desktop
+  @media (min-width: ${props => props.theme.mdToLg}) {
+    max-width: 33%;
+  }
+`;
 
-      a {
-        padding: 5px 5px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-decoration: none;
+export const StyledAnchor = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
 
-        &:hover,
-        &:focus {
-          background-color: transparent;
-        }
+  &:hover,
+  &:focus {
+    background-color: transparent;
+    cursor: pointer;
+  }
+`;
 
-        img {
-          width: 100%;
-          max-width: 100%;
-          margin-bottom: 5px;
-        }
-      }
-    }
+export const StyledImage = styled(Image)`
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 5px;
+  padding: 10px !important;
+  border: 1px solid rgba(0, 0, 0, 0.125) !important;
+  border-radius: 5px;
+
+  &:hover,
+  &:focus {
+    background-color: transparent;
+    opacity: 0.8;
   }
 `;
