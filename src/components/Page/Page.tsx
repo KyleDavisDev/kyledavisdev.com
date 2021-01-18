2ab83fc6b399f58c17b0d652990c0b55a0afb227
Kyle Bonar <kylewb2011@gmail.com>
2021-01-18 13:04:58 -0600

    Setup global styles as expected from App.tsx

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import {
   ThemeProvider,
   theme,
@@ -5,55 +7,63 @@ import {
 } from "../../theme/styled-components";
 import Meta from "../Meta/Meta";
 
-// const AvenirNextReg = "/static/AvenirNext-Regular.ttf";
-// const FuturaMedium = "/static/Futura-Medium.ttf";
+const DroidSansBold = "/static/fonts/DroidSans-Bold.ttf";
+const DroidSans = "/static/fonts/DroidSans.ttf";
+const Montserrat = "/static/fonts/Montserrat-Bold.ttf";
 
 interface IPageProps {
   children?: any;
 }
 
 const GlobalStyle = createGlobalStyle`
-  
-  /* Define the "system" font family */
   @font-face {
-    font-family: System;
-    font-style: normal;
-    font-weight: 300;
-    src: local(".SFNSText-Light"), local(".HelveticaNeueDeskInterface-Light"), local(".LucidaGrandeUI"), local("Ubuntu Light"), local("Segoe UI Light"), local("Roboto-Light"), local("DroidSans"), local("Tahoma");
+    font-family: DroidSansBold;
+    src: url('${DroidSansBold}') format('truetype');
+  }
+  @font-face {
+    font-family: DroidSans;
+    src: url('${DroidSans}') format('truetype');
+  }
+
+  @font-face {
+    font-family: Montserrat;
+    src: url('${Montserrat}') format('truetype');
+  }
+
+  html {
+    background-color: #fbfbfb;
+    font-family: DroidSans;
+  }
+
+  html,
+  body {
+    min-height: 100vh;
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    margin: 0px;
+    // font-family: DroidSans,Helvetica,Arial,sans-serif;
+  }
+
+
+  a{
+    color: #333;
+  }
+  a.active, a:hover, a:focus {
+    color: #00a0e3;
+    background-color: rgba(0,160,227,.05);
   }
 
   body {
-    margin: 0px;
-    min-height: 100vh;
+    background-color: #ebebeb;
+    padding: 0;
+    margin: 0;
   }
 
-  //a {
-  //  font-family: AvenirNextReg !important;
-  //}
-
-  //html {
-  //  background-color: #fbfbfb;
-  //  font-family: AvenirNextReg;
-  //  font-size: 16px;
-  //}
-
-  //h1,h2,h3,h4,h5,h6 {
-  //  margin: 0px;
-  //  font-family: FuturaMedium;
-  //}
-
-  #__next {
-    display: flex;
-    flex-direction:column;
-    justify-content: flex-start;
-    min-height: 100vh;
-
-    footer {
-      margin-top: auto;
-    }
+  strong, .bold {
+    font-weight: 700;
+    font-family: DroidSansBold,Helvetica,Arial,sans-serif;
   }
-
-
 `;
 
 const Page: React.FunctionComponent<IPageProps> = (props) => {
