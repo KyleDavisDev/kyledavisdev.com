43867925960543e424f0210be129218bd218ff21
Kyle Bonar <kylewb2011@gmail.com>
2021-01-18 17:36:46 -0600

    Use projects component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Nav from "../src/components/Nav/Nav";
 import Header from "../src/components/Header/Header";
 import AboutMe from "../src/components/AboutMe/AboutMe";
 // import ContactMe from "../src/components/ContactMe/ContactMe";
-// import Projects from "../src/components/Projects/Projects";
+import Projects from "../src/components/Projects/Projects";
 
 export interface IIndexProps {}
 
@@ -16,7 +16,7 @@ export default class Index extends React.Component<IIndexProps> {
         <Header />
         <AboutMe />
         {/*/!* <ContactMe /> *!/*/}
-        {/*<Projects />*/}
+        <Projects />
       </>
     );
   }
