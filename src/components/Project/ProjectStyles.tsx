2f87f43449d62c841e0f24f7d77417508a4e0911
Kyle Bonar <kylewb2011@gmail.com>
2021-01-24 14:32:16 -0600

    Update for single-column style

--- src/components/Project/ProjectStyles.tsx
+++ src/components/Project/ProjectStyles.tsx
@@ -2,49 +2,14 @@ import styled from "../../theme/styled-components";
 
 export const StyledProjectInfoContainer = styled.div`
   padding: 2em 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
-  max-width: 900px;
-
-  // desktop
-  @media (min-width: ${props => props.theme.mediumToLarge}) {
-    flex-direction: row;
-    align-items: flex-start;
-  }
-`;
-
-export const StyledImageContainer = styled.div`
-  box-sizing: border-box;
-  padding: 0 15px;
-  max-width: 350px !important;
   width: 100%;
-  flex: 1;
-  margin-bottom: 15px;
-
-  //tablets
-  @media (min-width: ${props => props.theme.extraSmallToSmall}) {
-  }
-
-  // desktop
-  @media (min-width: ${props => props.theme.mediumToLarge}) {
-    margin-bottom: 0;
-  }
-
-  img {
-    &:hover,
-    &:focus {
-      opacity: 0.8;
-    }
-  }
 `;
 
 export const StyledContentContainer = styled.div`
+  max-width: 900px;
   margin: 0 auto;
   box-sizing: border-box;
   width: 100%;
-  flex: 1;
   padding: 0 15px;
 
   @media (min-width: ${props => props.theme.extraSmallToSmall}) {
@@ -52,7 +17,6 @@ export const StyledContentContainer = styled.div`
 
   // desktop
   @media (min-width: ${props => props.theme.mediumToLarge}) {
-    flex: 3;
   }
 
   img {
