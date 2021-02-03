e525b05a222f18e5f3f0e72cacfcfe8fece0ee78
Kyle Bonar <kylewb2011@gmail.com>
2021-02-03 07:40:41 -0600

    Cleanup

--- src/Data/Data.tsx
+++ src/Data/Data.tsx
@@ -1,3 +1,5 @@
+import { tools, IToolsItem } from "./tools";
+
 const AdvancedReactCert = "/static/screenshots/AdvancedReactCert.png";
 const CustomWebIcons = "/static/screenshots/Custom-Icons-SS-min.png";
 const DoublePendulumSS = "/static/screenshots/DoublePendulum-SS-min.png";
@@ -12,44 +14,17 @@ const PingPongSS = "/static/screenshots/PingPong-SS-min.png";
 const SonsofVidya = "/static/screenshots/SonsofVidya-SS-min.png";
 const TamuAstronomySS = "/static/screenshots/Tamu-Astronomy-SS-min.png";
 const TamuPhysicsSS = "/static/screenshots/Tamu-Physics-SS-min.png";
-const TexasOakWiltSS = "/static/screenshots/Texas-Oak-Wilt-SS-min.png";
+const TexasOakWiltSS =
+  "/static/screenshots/TexasOakWilt/Texas-Oak-Wilt-SS-min.png";
 const SFYTSS = "/static/screenshots/SFYT-SS-min.png";
 const SickFitsSS = "/static/screenshots/SickFits-SS-min.png";
 const WordCounterSS = "/static/screenshots/WordCounter-SS-min.png";
-
-const AdobeIllustratorLogo = "/static/icons/Adobe-Illustrator-CC-Logo.png";
-const ApolloClientLogo = "/static/icons/ApolloClient-Logo.png";
-const BootstrapLogo = "/static/icons/Bootstrap-Logo.png";
-const CSS3Logo = "/static/icons/CSS3-Logo.png";
-const ExpressLogo = "/static/icons/ExpressJS-Logo.png";
-const FoundationLogo = "/static/icons/Foundation-Logo.png";
-const GitHubLogo = "/static/icons/GitHub-Logo.png";
-const HTML5Logo = "/static/icons/HTML5-Logo.png";
-const JSLogo = "/static/icons/JS-Logo.png";
-const JQueryLogo = "/static/icons/JQuery-Logo.png";
-const LinuxLogo = "/static/icons/Linux-Logo.png";
-const MySQLLogo = "/static/icons/MySQL-Logo.png";
-const NginxLogo = "/static/icons/Nginx-Logo.png";
-const NodeJSLogo = "/static/icons/NodeJS-Logo.png";
-const PHPLogo = "/static/icons/PHP-Logo.png";
-const PrismaLogo = "/static/icons/Prisma-Logo.png";
-const ReactJSLogo = "/static/icons/ReactJS-Logo.png";
-const ReduxLogo = "/static/icons/Redux-Logo.png";
-const SassLogo = "/static/icons/Sass-Logo.png";
-const StyledComponentsLogo = "/static/icons/StyledComponents-Logo.png";
-const TSLogo = "/static/icons/TS-Logo.png";
-const WordPressLogo = "/static/icons/Wordpress-Logo.png";
-const YogaLogo = "/static/icons/Yoga-Logo.png";
 
 export interface IProjectImageItem {
   path: string;
   alt?: string;
 }
-export interface IToolsItem {
-  name: string;
-  img: string;
-  link?: string;
-}
+
 export interface IExternalData {
   isActive: boolean;
   link: string;
@@ -69,113 +44,6 @@ export interface IProjectItem {
   images: Array<IProjectImageItem>;
   external: IExternalData;
 }
-
-const tools: { [name: string]: IToolsItem } = {
-  AdobeIllustrator: {
-    name: "Adobe Illustrator CC",
-    img: AdobeIllustratorLogo,
-    link: "https://www.adobe.com/products/illustrator.html"
-  },
-  ApolloClient: {
-    name: "Apollo Client",
-    img: ApolloClientLogo,
-    link: "https://www.apollographql.com/docs/react/"
-  },
-  Bootstrap3: {
-    name: "Bootstrap CSS Library",
-    img: BootstrapLogo,
-    link: "https://getbootstrap.com/docs/3.3/"
-  },
-  CSS3: {
-    name: "CSS3",
-    img: CSS3Logo,
-    link: "https://developer.mozilla.org/en-US/docs/Web/css"
-  },
-  ExpressJS: {
-    name: "ExpressJS Routing",
-    img: ExpressLogo,
-    link: "https://expressjs.com/"
-  },
-  Foundation: {
-    name: "Foundation Zurb CSS library",
-    img: FoundationLogo,
-    link: "https://get.foundation/"
-  },
-  GitHub: {
-    name: "GitHub",
-    img: GitHubLogo,
-    link: "https://github.com/"
-  },
-  GraphQLYoga: {
-    name: "GraphQL Yoga",
-    img: YogaLogo,
-    link: "https://github.com/prisma-labs/graphql-yoga"
-  },
-  HTML5: {
-    name: "HTML5",
-    img: HTML5Logo,
-    link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
-  },
-  JavaScript: {
-    name: "JavaScript",
-    img: JSLogo,
-    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-  },
-  JQuery: {
-    name: "JQuery JavaScript library",
-    img: JQueryLogo,
-    link: "https://jquery.com/"
-  },
-  Linux: {
-    name: "Linux Server Language",
-    img: LinuxLogo,
-    link: "https://www.linux.org/"
-  },
-  MySQL: {
-    name: "MySQL database",
-    img: MySQLLogo,
-    link: "https://www.mysql.com/"
-  },
-  Nginx: {
-    name: "Nginx Web Server",
-    img: NginxLogo,
-    link: "https://www.nginx.com/"
-  },
-  NodeJS: {
-    name: "NodeJS Server Language",
-    img: NodeJSLogo,
-    link: "https://nodejs.org/en/"
-  },
-  PHP7: {
-    name: "PHP 7.3 Server Language",
-    img: PHPLogo,
-    link: "https://www.php.net/"
-  },
-  PHP5: {
-    name: "PHP 5.4 Server Language",
-    img: PHPLogo,
-    link: "https://www.php.net/"
-  },
-  Prisma: { name: "Prisma", img: PrismaLogo, link: "https://www.prisma.io/s" },
-  React: { name: "React", img: ReactJSLogo, link: "https://reactjs.org/" },
-  Redux: { name: "Redux", img: ReduxLogo, link: "https://redux.js.org/" },
-  Sass: { name: "Sass CSS", img: SassLogo, link: "https://sass-lang.com/" },
-  StyledComponents: {
-    name: "Styled Components styling",
-    img: StyledComponentsLogo,
-    link: "https://styled-components.com/"
-  },
-  TypeScript: {
-    name: "TypeScript",
-    img: TSLogo,
-    link: "https://www.typescriptlang.org/"
-  },
-  Wordpress: {
-    name: "Wordpress",
-    img: WordPressLogo,
-    link: "https://wordpress.org/"
-  }
-};
 
 const ProjectList: IProjectItem[] = [
   {
@@ -197,7 +65,14 @@ const ProjectList: IProjectItem[] = [
       tools.CSS3,
       tools.Linux
     ],
-    images: [{ path: TexasOakWiltSS }],
+    images: [
+      { path: TexasOakWiltSS },
+      { path: "/static/screenshots/TexasOakWilt/FindMyForester.png" },
+      { path: "/static/screenshots/TexasOakWilt/RecommendedTrees.png" },
+      { path: "/static/screenshots/TexasOakWilt/Vendors.png" },
+      { path: "/static/screenshots/TexasOakWilt/OakwiltIdentification.png" },
+      { path: "/static/screenshots/TexasOakWilt/FAQs.png" }
+    ],
     external: {
       isActive: true,
       link: "https://texasoakwilt.org",
