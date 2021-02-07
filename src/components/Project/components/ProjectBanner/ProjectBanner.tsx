1b0f6c995bc5dc6bf8b10da861bfca31287c41bb
Kyle Bonar <kylewb2011@gmail.com>
2021-02-07 13:46:03 -0600

    anchor to next link

--- src/components/Project/components/ProjectBanner/ProjectBanner.tsx
+++ src/components/Project/components/ProjectBanner/ProjectBanner.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Link from "next/link";
 import BannerBar from "../../../BannerBar/BannerBar";
 
 import { StyledLink } from "./ProjectBannerStyles";
@@ -17,7 +18,9 @@ const ProjectBanner: React.FC<IProjectBanner> = props => {
         <h2>
           {name} {year && `(${year})`}
         </h2>
-        <StyledLink href="/">&larr; Back</StyledLink>
+        <Link href={"/"}>
+          <StyledLink>&larr; Back</StyledLink>
+        </Link>
       </div>
       {isActive && (
         <StyledLink href={link} target="_blank" rel="nofollow">
