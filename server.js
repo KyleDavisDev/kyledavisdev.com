4ab0d3eb468422e293ea8a183942bb86e34ed15c
Kyle Bonar <kylewb2011@gmail.com>
2021-03-22 22:49:58 -0500

    Apply prettier

--- server.js
+++ server.js
@@ -7,35 +7,35 @@ app.use(express.static("KyleBonar_2015/"));
 // app.set('views', './build');
 
 //Send Resume
-app.get("/resume", function(req, res) {
+app.get("/resume", function (req, res) {
   res.sendFile(`${process.cwd()}/src/images/profile/Davis_Kyle_09FEB2020.pdf`);
 });
 
 //Send specific JavaScript game
-app.get("/JSGames/doublePend", function(req, res) {
+app.get("/JSGames/doublePend", function (req, res) {
   res.sendFile(`${process.cwd()}/JSGames/doublePend/index.html`);
 });
-app.get("/JSGames/fractalTrees", function(req, res) {
+app.get("/JSGames/fractalTrees", function (req, res) {
   res.sendFile(`${process.cwd()}/JSGames/fractalTrees/index.html`);
 });
-app.get("/JSGames/phyllotaxis", function(req, res) {
+app.get("/JSGames/phyllotaxis", function (req, res) {
   res.sendFile(`${process.cwd()}/JSGames/phyllotaxis/index.html`);
 });
-app.get("/JSGames/pong", function(req, res) {
+app.get("/JSGames/pong", function (req, res) {
   res.sendFile(`${process.cwd()}/JSGames/pong/index.html`);
 });
-app.get("/JSGames/wordCount", function(req, res) {
+app.get("/JSGames/wordCount", function (req, res) {
   res.sendFile(`${process.cwd()}/JSGames/wordCount/index.html`);
 });
-app.get("/KyleBonar_2015", function(req, res) {
+app.get("/KyleBonar_2015", function (req, res) {
   res.sendFile(`${process.cwd()}/KyleBonar_2015/index.html`);
 });
 
 //rest will fall here and be handled by react app
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(`${process.cwd()}/dist/index.html`);
 });
 
-app.listen(5000, function() {
+app.listen(5000, function () {
   console.log("Example app listening on port 5000!");
 });
