b58f8b18a0e30bfe2d845629d837a7c47433cd26
Kyle Davis <kyledavisdev@gmail.com>
2022-03-16 23:07:40 -0500

    Update verbiage

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -9,21 +9,22 @@ const AboutMe: React.FunctionComponent<IAboutMeProps> = props => {
     <StyledContainer>
       <StyledTextContainer>
         <SectionTitle>About me</SectionTitle>
-        <p>Hello world! Welcome to my portfolio website.</p>
+        <p>Welcome to my portfolio website.</p>
 
         <p>
-          I am a <span className="bold">Software Engineer</span> that enjoys
+          I am a <span className="bold">Software Engineer</span> who enjoys
           working with JavaScript and C# as much as possible. Often, however,
-          you will find me learning new languages and technologies as well!
+          you will find me learning new languages and technologies! Lately, I have
+          been playing around with React Native.
         </p>
 
         <p>
           I primarily focus on the front-end, where I make pixel-perfect,
           minimalistic designs, but I also enjoy working with databases and
-          server logic. The majority of my projects are internal tooling (behind
-          login pages) and so I cannot demo them. The project section below,
-          however, include some of my projects I've created in my off time
-          either for fun or for learning!
+          server logic. The majority of my work is internal tooling
+          (behind login pages), so I cannot demo them here; however, the
+          project section below includes some of the projects I've created
+          in my free time, either for fun or for self-learning!
         </p>
 
         <p>
