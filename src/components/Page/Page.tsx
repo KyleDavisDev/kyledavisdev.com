c0dbc85b1a2a13ef9c145efa9ae10b2192e0d6fb
Kyle Davis <kyledavisdev@gmail.com>
2024-05-06 20:48:11 -0500

    Correct font imports

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,68 +1,62 @@
 import * as React from "react";
 
-import {
-  ThemeProvider,
-  theme,
-  createGlobalStyle
-} from "../../theme/styled-components";
+import { ThemeProvider, theme, createGlobalStyle } from "../../theme/styled-components";
 import Meta from "../Meta/Meta";
-
-const DroidSansBold = "/static/fonts/DroidSans-Bold.ttf";
-const DroidSans = "/static/fonts/DroidSans.ttf";
-const Montserrat = "/static/fonts/Montserrat-Bold.ttf";
 
 interface IPageProps {
   children?: any;
 }
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: DroidSansBold;
-    src: url('${DroidSansBold}') format('truetype');
-  }
-  @font-face {
-    font-family: DroidSans;
-    src: url('${DroidSans}') format('truetype');
-  }
+    @font-face {
+        font-family: DroidSansBold;
+        src: url('/fonts/DroidSans-Bold.ttf') format('truetype');
+    }
 
-  @font-face {
-    font-family: Montserrat;
-    src: url('${Montserrat}') format('truetype');
-  }
+    @font-face {
+        font-family: DroidSans;
+        src: url('/fonts/DroidSans.ttf') format('truetype');
+    }
 
-  html {
-    background-color: #fbfbfb;
-    font-family: DroidSans;
-  }
+    @font-face {
+        font-family: Montserrat;
+        src: url('/fonts/Montserrat-SemiBold.ttf') format('truetype');
+    }
 
-  html,
-  body {
-    min-height: 100vh;
-  }
+    html {
+        background-color: #fbfbfb;
+        font-family: DroidSans;
+    }
 
-  h1,h2,h3,h4,h5,h6 {
-    margin: 0px;
-  }
+    html,
+    body {
+        min-height: 100vh;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        margin: 0px;
+    }
 
 
-  a {
-    color: ${x => x.theme.anchorColor};
-  }
-  a.active, a:hover, a:focus {
-    color: ${x => x.theme.anchorHoverColor};;
-    background-color: rgba(0,160,227,.05);
-  }
+    a {
+        color: ${x => x.theme.anchorColor};
+    }
 
-  body {
-    background-color: #ebebeb;
-    padding: 0;
-    margin: 0;
-  }
+    a.active, a:hover, a:focus {
+        color: ${x => x.theme.anchorHoverColor};;
+        background-color: rgba(0, 160, 227, .05);
+    }
 
-  strong, .bold {
-    font-weight: 700;
-    font-family: DroidSansBold,Helvetica,Arial,sans-serif;
-  }
+    body {
+        background-color: #ebebeb;
+        padding: 0;
+        margin: 0;
+    }
+
+    strong, .bold {
+        font-weight: 700;
+        font-family: DroidSansBold, Helvetica, Arial, sans-serif;
+    }
 `;
 
 const Page: React.FunctionComponent<IPageProps> = props => {
