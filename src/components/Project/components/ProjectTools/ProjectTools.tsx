e9585c93b882145d0698d7d43910ca56e75e5ccf
Kyle Davis <kyledavisdev@gmail.com>
2024-05-08 00:01:05 -0500

    Do not render section if no tools

--- src/components/Project/components/ProjectTools/ProjectTools.tsx
+++ src/components/Project/components/ProjectTools/ProjectTools.tsx
@@ -10,6 +10,8 @@ export interface IProjectTools {
 const ProjectTools: React.FC<IProjectTools> = props => {
   const { tools } = props;
 
+  if (tools.length === 0) return <></>;
+
   return (
     <div>
       <SectionHeader>Tools</SectionHeader>
