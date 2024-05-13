b73cc38b67be2ba42f68e76e3c21f2a9937ecec7
Kyle Davis <kyledavisdev@gmail.com>
2024-05-12 20:33:31 -0500

    Update verbiage

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -12,25 +12,19 @@ const AboutMe: React.FunctionComponent<IAboutMeProps> = props => {
         <p>Welcome to my portfolio website.</p>
 
         <p>
-          I am a <span className="bold">Software Engineer</span> who enjoys
-          working with JavaScript and C# as much as possible. Often, however,
-          you will find me learning new languages and technologies! Lately, I have
-          been playing around with React Native.
+          I am a <span className="bold">Software Engineer</span> who enjoys working with NodeJS and Java as much as
+          possible. I especially enjoy working with TypeScript! Often times, however, you will find me learning new
+          languages and technologies -- lately, I have been playing around with React Native.
         </p>
 
         <p>
-          I primarily focus on the front-end, where I make pixel-perfect,
-          minimalistic designs, but I also enjoy working with databases and
-          server logic. The majority of my work is internal tooling
-          (behind login pages), so I cannot demo them here; however, the
-          project section below includes some of the projects I've created
-          in my free time, either for fun or for self-learning!
+          While I used to do a lot of full-stack work (for over 7 years!), I now primarily focus on the backend-end side
+          of the stack such as designing (and building) performant APIs, working on business logic and architecting
+          databases. The majority of my professional work private, and so I cannot demo them here, however, the project
+          section below includes some of the projects I've created in my free time, either for fun or for self-learning!
         </p>
 
-        <p>
-          The majority of the updates to this site will be focused on the
-          projects section. Stay tuned!
-        </p>
+        <p>The majority of the updates to this site will be focused on the projects section. Stay tuned!</p>
 
         <p>-Kyle Davis</p>
       </StyledTextContainer>
